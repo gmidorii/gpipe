fb7a4b30fa9cfc8d21398d801c7106e6006a9b05
tagasouichiro <stitow14@gmail.com>
2018-07-17 23:37:06 +0900

    Fix pipe

---
+++ cmd/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/midorigreen/gpipe"
+)
+
+type InputType struct {
+	ID   int
+	Name string
+}
+
+func (i InputType) String() string {
+	return fmt.Sprintf("%v, %v", i.ID, i.Name)
+}
+
+type OutputType struct {
+	ID        int
+	Name      string
+	UpperName string
+}
+
+func (o OutputType) String() string {
+	return fmt.Sprintf("%v, %v, %v", o.ID, o.Name, o.UpperName)
+}
+
+type Upper struct{}
+
+func (u Upper) Convert(out fmt.Stringer, params []interface{}) (fmt.Stringer, error) {
+	o, ok := out.(InputType)
+	if !ok {
+		return nil, fmt.Errorf("cast error")
+	}
+	return OutputType{
+		ID:        o.ID,
+		Name:      o.Name,
+		UpperName: strings.ToUpper(o.Name),
+	}, nil
+}
+
+func main() {
+	ctx, _ := context.WithCancel(context.Background())
+	in := gpipe.Input(os.Stdin, 10)
+	convs := []gpipe.Converter{
+		Upper{},
+	}
+	gpipe.Output(ctx, os.Stdout, in, parse, convs)
+}
+
+func parse(b []byte) (fmt.Stringer, error) {
+	r := csv.NewReader(bytes.NewBuffer(b))
+	columns, err := r.Read()
+	if err != nil {
+		return InputType{}, err
+	}
+	id, err := strconv.Atoi(columns[0])
+	if err != nil {
+		return InputType{}, err
+	}
+	return InputType{
+		ID:   id,
+		Name: columns[1],
+	}, nil
+}
--- gpipe.go
+++ gpipe.go
@@ -2,14 +2,16 @@ package gpipe
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 )
 
-// Parser is parse func interface
-type Parser interface {
-	//Parse(param []interface{}) (interface{}, error)
-	Parse(in interface{}) (interface{}, error)
+type Parse func(b []byte) (fmt.Stringer, error)
+
+// Converter is parse func interface
+type Converter interface {
+	Convert(out fmt.Stringer, params []interface{}) (fmt.Stringer, error)
 }
 
 // Input is createing input reader
@@ -18,6 +20,7 @@ func Input(r io.Reader, size int) chan []byte {
 	in := make(chan []byte, size)
 	go func(r io.Reader, in chan []byte) {
 		defer close(in)
+
 		scanner := bufio.NewScanner(r)
 		for scanner.Scan() {
 			in <- scanner.Bytes()
@@ -30,23 +33,27 @@ func Input(r io.Reader, size int) chan []byte {
 }
 
 // Output is output to io.Writer
-func Output(w io.Writer, in chan []byte, conv func(b []byte) interface{}, parsers []Parser) error {
+func Output(ctx context.Context, w io.Writer, in chan []byte, parse Parse, converters []Converter) error {
 	for {
 		select {
 		case b, ok := <-in:
 			if !ok {
+				return nil
+			}
+			out, err := parse(b)
+			if err != nil {
 				break
 			}
-			out := conv(b)
-			for _, p := range parsers {
-				output, err := p.Parse(out)
+			for _, p := range converters {
+				output, err := p.Convert(out, nil)
 				if err != nil {
 					return err
 				}
 				out = output
 			}
-			w.Write(out.([]byte))
+			fmt.Fprintln(w, out.String())
+		case <-ctx.Done():
+			return nil
 		}
 	}
-	return nil
 }
